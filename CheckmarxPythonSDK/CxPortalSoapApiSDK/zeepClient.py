86a5cd0d3b4d5098718b5974d3c971c1f2b25d0d
cx-happy-yang <30431255+cx-happy-yang@users.noreply.github.com>
2026-04-03 14:26:45 +0800

    [Fix] retry issue

--- CheckmarxPythonSDK/CxPortalSoapApiSDK/zeepClient.py
+++ CheckmarxPythonSDK/CxPortalSoapApiSDK/zeepClient.py
@@ -8,7 +8,7 @@ from CheckmarxPythonSDK.configuration import Configuration
 from CheckmarxPythonSDK.CxPortalSoapApiSDK.config import construct_configuration
 
 
-def retry_when_unauthorized(max_retries: int = 1):
+def retry(max_retries: int = 1):
     """
     Decorator that retries the HTTP request when receiving a 401 Unauthorized response.
     """
@@ -30,6 +30,7 @@ def retry_when_unauthorized(max_retries: int = 1):
                          or '12563' in response.ErrorMessage
                          or 'Invalid_Token' in response.ErrorMessage):
                     self.token_manager.refresh_token()
+                    self.client.settings.extra_http_headers["Authorization"] = f'Bearer {self.token_manager.get_token()}'
                 retries += 1
             raise Exception("Max retries exceeded")
 
@@ -44,6 +45,7 @@ class ZeepClient(ApiClient):
         if configuration is None:
             configuration = construct_configuration()
         ApiClient.__init__(self, configuration=configuration)
+        self.session.headers["Authorization"] = f'Bearer {self.token_manager.get_token()}'
         self.client = Client(
             wsdl=f"{configuration.server_base_url}{relative_web_interface_url}",
             transport=Transport(session=self.session),
@@ -55,6 +57,6 @@ class ZeepClient(ApiClient):
         )
         self.factory = self.client.type_factory("ns0")
 
-    @retry_when_unauthorized(max_retries=1)
+    @retry(max_retries=2)
     def execute(self, operation_name: str, *args, **kwargs):
         return self.client.service[operation_name](*args, **kwargs)
